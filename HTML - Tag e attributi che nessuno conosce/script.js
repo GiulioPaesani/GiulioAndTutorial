f1c7eced75b8399cfa13dedf84895e183ddb4d67
GiulioPaesani <57570629+GiulioPaesani@users.noreply.github.com>
2021-10-26 19:34:55 +0200

    Tag e attributi che nessuno conosce

---
+++ HTML - Tag e attributi che nessuno conosce/script.js
@@ -0,0 +1,15 @@
+function getColor() {
+    console.log(document.getElementById("inputColor").value)
+}
+
+function getValore() {
+    console.log(document.getElementById("inputTesto").value)
+}
+
+function cambiaValore() {
+    document.getElementById("progresso").value = 50
+}
+
+function prendiValore() {
+    console.log(document.getElementById("testoEditabile").innerText)
+}
